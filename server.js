b769e5d9247c55f3f65ce320579744b2499462d1
Carson Johnson <carson74johnson@gmail.com>
2021-10-22 16:12:10 -0400

    required express and made sure server runs

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
