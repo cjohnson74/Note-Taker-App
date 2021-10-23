ae2cf5fe2c686638043ad3768f1910f754da9fcd
Carson Johnson <carson74johnson@gmail.com>
2021-10-22 23:02:07 -0400

    notes are saved and wrote to the db.json file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,14 +4,21 @@ const fs = require('fs');
 
 // gets the notes that are saved
 router.get('/notes', (req, res) => {
-    res.send(JSON.parse(fs.readFileSync('./db/db.json')));
+    res.send(fs.readFileSync('./db/db.json', 'utf8'));
 })
 
 // posts to SAVE the note
 router.post('/notes', (req, res) => {
-    // var prevNotes = JSON.parse(fs.readFile('./db/db.json'))
-    // prevNotes.push(JSON.parse(req.body))
-})
+    const prevNotes = fs.readFileSync('./db/db.json', 'utf8');
+    const currentNotes = JSON.parse(prevNotes);
+    currentNotes.push(req.body);
+    // const prevNotes = prevNotes.push(JSON.parse(req.body));
+    // currentNotes = JSON.parse(JSON.stringify(currentNotes));
+    fs.writeFileSync('./db/db.json', JSON.stringify(currentNotes));
+    // prevNotes.push(JSON.parse(JSON.stringify(req.body)));
+    // JSON.parse(JSON.stringify(prevNotes));
+    // fs.writeFileSync('./db/db.json', `${prevNotes}`);
+});
 
 // // deletes the note
 // require.delete('/notes', (req, res) => {
